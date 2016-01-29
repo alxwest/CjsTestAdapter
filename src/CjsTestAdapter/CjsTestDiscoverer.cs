5b36b4b318192cc200546aebe1238f705e77ce12
alxwest <alxwest@gmail.com>
2016-01-29 15:28:10 +0000

    test

--- src/CjsTestAdapter/CjsTestDiscoverer.cs
+++ src/CjsTestAdapter/CjsTestDiscoverer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
